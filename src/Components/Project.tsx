659faadfd8cbfc5b1e692e8721d80b8a59536aaf
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-07-23 13:34:28 +0200

    feat:add project section

---
+++ src/Components/Project.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    <div>
+      <div id="project" className="w-full items-center "></div>
+      <div className="relative bg-gray-900 flex h-screen w-full items-center justify-center">
+        <h1 className=" text-center text-gray-500 xl:text-center text-xl ">
+          My Project
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Project
