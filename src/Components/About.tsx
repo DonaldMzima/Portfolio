eb2f3c45487ea3fee765da12d187683970c0a044
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-08-24 16:14:41 +0200

    ref:make about section mobile responsive

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -9,9 +9,8 @@ const SkillContainer = styled.div`
   align-items: center;
   justify-content: center;
   display: grid;
-  height: 100vh;
-  grid-template-columns: repeat(2, 1fr); /* Create 5 equal columns */
-  grid-gap: 10px;
+  height: auto; /* Change to auto height for responsiveness */
+  grid-template-columns: repeat(1, 1fr); /* Change to 1 column for mobile */
   list-style: none;
   padding: 0;
 `
@@ -26,16 +25,16 @@ const About = () => {
   return (
     <div id="about">
       <Title> About Me</Title>
-      {/* <div className="bg-gray-900 h-screen w-full flex items-center justify-center"> */}
       <SkillContainer>
         <div className="md:w-1/2">
-          <p className="text-gray-500 max-w-xs md:ml-6">
-            I am a frontend developer, primarily working on web development. I
-            have gained my skills through years of hard work and experience.
-            Starting as a Junior developer, I have worked at Joblify-Connect and
-            have progressed through various technologies, making me versatile
-            and adaptable. I am passionate about learning and problem-solving,
-            and currently seeking opportunities with ambitious and innovative
+          <p className="text-gray-500 max-w-xs md:ml-6 md:max-w-md lg:max-w-lg">
+            {/* Apply responsive text and margin classes */}I am a frontend
+            developer, primarily working on web development. I have gained my
+            skills through years of hard work and experience. Starting as a
+            Junior developer, I have worked at Joblify-Connect and have
+            progressed through various technologies, making me versatile and
+            adaptable. I am passionate about learning and problem-solving, and
+            currently seeking opportunities with ambitious and innovative
             companies to contribute to their goals.
           </p>
           <div>
@@ -45,12 +44,12 @@ const About = () => {
             </a>
           </div>
         </div>
-        <div className="max-w-3xl flex flex-col ">
+        <div className="max-w-full flex flex-col md:mt-6">
+          {/* Apply margin-top class for spacing */}
           <AboutSvg />
         </div>
       </SkillContainer>
     </div>
-    // </div>
   )
 }
 
