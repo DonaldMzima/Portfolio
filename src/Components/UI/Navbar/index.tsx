5490ee027ceae9c16b838040e2d3ebb6dcb09906
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-08-25 16:21:14 +0200

    ref:make a navba responsive

--- src/Components/UI/Navbar/index.tsx
+++ src/Components/UI/Navbar/index.tsx
@@ -1,95 +1,113 @@
 import React, { useState } from 'react'
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-  const toggleMenu = () => setIsOpen(!isOpen)
+  const toggleMenu = () => {
+    setIsMenuOpen(true)
+  }
 
   return (
-    <nav className="bg-gray-800 fixed w-full z-10">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button
-              type="button"
-              onClick={toggleMenu}
-              className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded={isOpen}
+    <nav className="bg-gray-800 fixed w-full z-10 p-4">
+      <div className="flex items-center justify-between">
+        <div className="text-gray-300 font-bold text-xl">Donald Mzima.</div>
+        <div className="md:hidden">
+          <button
+            onClick={toggleMenu}
+            type="button"
+            className="text-white focus:outline-none"
+          >
+            <svg
+              className="h-6 w-6 fill-current"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className={`h-6 w-6 ${isOpen ? 'hidden' : 'block'}`}
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
+              {isMenuOpen ? (
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M3 5H21V7H3V5ZM3 11H21V13H3V11ZM3 17H21V19H3V17Z"
                 />
-              </svg>
-              <svg
-                className={`h-6 w-6 ${isOpen ? 'block' : 'hidden'}`}
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
+              ) : (
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 5H20V7H4V5ZM4 11H20V13H4V11ZM4 17H20V19H4V17Z"
                 />
-              </svg>
-            </button>
-          </div>
-          <div className="flex flex-1  sm:items-stretch md:justify-start text-2xl font-semibold whitespace-nowrap dark:text-white">
-            <div className="flex flex-shrink-0 text-gray-500 i justify-start ">
-              <h1 className="justify-start">Donald Mzima</h1>
-            </div>
-            <div className={`sm:ml-6 sm:block ${isOpen ? 'block' : 'hidden'}`}>
-              <div className="space-x-4 flex flex-1 items-center justify-center">
-                <a
-                  href="#"
-                  className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-                <a
-                  href="#about"
-                  className=" bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >
-                  About
-                </a>
-                <a
-                  href="#skills"
-                  className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >
-                  Skills
-                </a>
-                <a
-                  href="#project"
-                  className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >
-                  Projects
-                </a>
-                <a
-                  href="#contact"
-                  className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >
-                  Contacts
-                </a>
-              </div>
-            </div>
-          </div>
+              )}
+            </svg>
+          </button>
+        </div>
+        <div className="hidden md:flex space-x-4 flex-1 items-center justify-center">
+          <a
+            href="#"
+            className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+            aria-current="page"
+          >
+            Home
+          </a>
+          <a
+            href="#about"
+            className=" bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          >
+            About
+          </a>
+          <a
+            href="#skills"
+            className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          >
+            Skills
+          </a>
+          <a
+            href="#project"
+            className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          >
+            Projects
+          </a>
+          <a
+            href="#contact"
+            className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          >
+            Contacts
+          </a>
         </div>
       </div>
+      {/* Mobile menu */}
+      {isMenuOpen && (
+        <div className="md:hidden mt-2">
+          <a
+            href="#"
+            className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+            aria-current="page"
+          >
+            Home
+          </a>
+          <a
+            href="#about"
+            className=" bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          >
+            About
+          </a>
+          <a
+            href="#skills"
+            className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          >
+            Skills
+          </a>
+          <a
+            href="#project"
+            className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          >
+            Projects
+          </a>
+          <a
+            href="#contact"
+            className="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          >
+            Contacts
+          </a>
+        </div>
+      )}
     </nav>
   )
 }
