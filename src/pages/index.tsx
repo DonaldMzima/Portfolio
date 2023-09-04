da529b75f1463bc0fabf5eefee397f5ca7ec97e6
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-09-04 16:32:45 +0200

    ref:add ScrollToTopButton component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,31 +5,39 @@ import Intro from '@/Components/AutoWriteText/Intro'
 import Footer from '@/Components/Footer'
 import Skill from '@/Components/Skill'
 import Project from '@/Components/Project'
+import ScrollToTopButton from '@/Components/Buttons/ScrollToTopButton'
 
 export default function Home() {
   return (
     <>
       <div>
         <Navbar />
+        <ScrollToTopButton />
       </div>
       <div className="sm:px-4 md:px-8 lg:px-16 bg-gray-800">
         <Intro />
+        <ScrollToTopButton />
       </div>
       <div className="sm:px-4 md:px-8 lg:px-16 bg-gray-300">
         <About />
+        <ScrollToTopButton />
       </div>
       <div className="sm:px-4 md:px-8 lg:px-16 bg-gray-800">
         <Skill />
+        <ScrollToTopButton />
       </div>
       <div className="sm:px-4 md:px-8 lg:px-16 bg-gray-300">
         <Project />
+        <ScrollToTopButton />
       </div>
 
       <div className="sm:px-4 md:px-8 lg:px-16 bg-gray-800">
         <ContactForm />
+        <ScrollToTopButton />
       </div>
       <div className="sm:px-4 md:px-8 lg:px-16 bg-gray-800">
         <Footer />
+        <ScrollToTopButton />
       </div>
     </>
   )
