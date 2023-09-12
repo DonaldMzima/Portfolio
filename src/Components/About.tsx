4e14822c34ae42ab4ee06c734a40699d20949cac
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2023-09-12 11:11:53 +0200

    ref:delete styled component on the  about page

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,50 +1,33 @@
 import React from 'react'
 import AboutSvg from './Svg/AboutSvg'
-import styled from '@emotion/styled'
 import ResumeButton from './Buttons/ResumeButton'
 import ScrollToTopButton from './Buttons/ScrollToTopButton'
 
-const SkillContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(1, 1fr);
-  gap: 20px;
-  align-items: center;
-  justify-content: center;
-  list-style: none;
-  padding: 0;
-  min-height: 80vh;
-  @media (min-width: 768px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
+const SkillContainerStyles = `
+  grid grid-cols-1 gap-20 items-center justify-center list-none p-0 min-h-screen
+  md:grid-cols-2
 `
 
-const Title = styled.h1`
-  color: black;
-  font-size: 34px;
-  text-align: center;
-  text-decoration: underline;
+const TitleStyles = `
+  text-black text-3xl text-center underline
 `
 
-const ResumeButtonContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 90px;
+const ResumeButtonContainerStyles = `
+  flex items-center ml-16
 `
 
-const ResumeTitle = styled.span`
-  color: white;
-  margin-left: -63px;
-  margin-top: 55px;
+const ResumeTitleStyles = `
+  text-white ml-[-63px] mt-16
 `
 
 const About = () => {
   return (
     <div id="about">
       <ScrollToTopButton />
-      <Title> About Me</Title>
-      <SkillContainer>
+      <h1 className={TitleStyles}> About Me</h1>
+      <div className={SkillContainerStyles}>
         <div className="md:w-1/2">
-          <p className="text-gray-800 max-w-xs md:ml-6 md:max-w-md lg:max-w-lg">
+          <p className="text-gray-800 max-w-xs ml-6 md:max-w-md lg:max-w-lg">
             I am a frontend developer, primarily working on web development. I
             have gained my skills through years of hard work and experience.
             Starting as a Junior developer, I have worked at Joblify-Connect and
@@ -53,17 +36,15 @@ const About = () => {
             and currently seeking opportunities with ambitious and innovative
             companies to contribute to their goals.
           </p>
-          <div>
-            <ResumeButtonContainer>
-              <ResumeButton />
-              <ResumeTitle>Get Resume</ResumeTitle>
-            </ResumeButtonContainer>
+          <div className={ResumeButtonContainerStyles}>
+            <ResumeButton />
+            <span className={ResumeTitleStyles}>Get Resume</span>
           </div>
         </div>
         <div className="max-w-full">
           <AboutSvg />
         </div>
-      </SkillContainer>
+      </div>
     </div>
   )
 }
